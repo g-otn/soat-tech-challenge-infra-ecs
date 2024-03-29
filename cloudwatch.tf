a56d033c8741fdf2e563bb7c5a8e91d37e01e0c2
g-otn <44736064+g-otn@users.noreply.github.com>
2024-03-29 16:40:27 -0300

    fix: duplicated cloudwatch log group

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,14 +1,4 @@
 #tfsec:ignore:aws-cloudwatch-log-group-customer-key
-resource "aws_cloudwatch_log_group" "api_gateway_access_log" {
-  name              = "/aws/apigateway/SOAT-TC_API_Gateway_Access_Log"
-  retention_in_days = 30
-
-  tags = {
-    Name : "SOAT-TC API GW Default Stage Access Log Cloudwatch Log Group"
-  }
-}
-
-#tfsec:ignore:aws-cloudwatch-log-group-customer-key
 resource "aws_cloudwatch_log_group" "lambda_authorizer_client" {
   name              = "/aws/lambda/SOAT-TC_Lambda_Authorizer_Client_Logs"
   retention_in_days = 30
